98d7c996530ec55f3cf6d2bf0428307df7225d55
DobleTmatutin0 <zz_00024@hotmail.com>
2026-03-23 18:14:38 -0300

    activates plays route and plays-detail route

--- frontend/cli/src/app/app.routes.ts
+++ frontend/cli/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home.component';
+import { PlaysComponent } from './plays.component/plays.component';
+import { PlaysDetailComponent } from './plays.component/plays-detail.component';
 
 export const routes: Routes = [
-    {path: '', component: Home}
+    { path: '', component: Home },
+    { path: 'plays', component: PlaysComponent },
+    { path: 'plays/:id', component: PlaysDetailComponent }
 ];
