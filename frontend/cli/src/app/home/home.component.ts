abb225814b6cb8c920c93dffd0be430e3ee1ff50
DobleTmatutin0 <zz_00024@hotmail.com>
2026-03-23 15:57:40 -0300

    fin video uno de juanma

---
+++ frontend/cli/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: ` 
+    <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Bienvenido a print the bill</h1>
+        <p>este es el frontend de nuestra app</p>
+    </div>
+  `,
+  styles: ``,
+})
+export class Home {}
