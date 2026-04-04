805633ab55455d0b9e048e3c3a2d19510b0a815b
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-03 23:30:49 -0300

    modifies the component to use the data from the backend

--- frontend/cli/src/app/plays.component/plays.component.ts
+++ frontend/cli/src/app/plays.component/plays.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { MOCK_PLAYS } from './mock-plays';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { PlayService } from './play.service';
+import { Play } from '../models/play';
 
 @Component({
     selector: 'app-plays.component',
@@ -15,19 +16,19 @@ import { RouterModule } from '@angular/router';
             <table class="table table-striped table-sm">
                 <thead>
                     <th>#</th>
+                    <th>Codigo</th>
                     <th>Nombre</th>
                     <th>Tipo</th>
-                    <th>Codigo</th>
                     <th></th>
                 </thead>
                 <tbody>
                     <tr *ngFor= "let play of plays; index as i">
                         <td>{{ i + 1}}</td>
-                        <td>{{ play.name }}</td>
-                        <td>{{ play.type }}</td>
                         <td>{{ play.code }}</td>
+                        <td>{{ play.name }}</td>
+                        <td>{{ play.type.type }}</td>
                         <td>
-                            <a routerLink="/plays/{{play.id}}">
+                            <a routerLink="/plays/{{play.code}}">
                                 <i class="fa-solid fa-pencil"></i>
                             </a>
                         </td>
@@ -39,7 +40,17 @@ import { RouterModule } from '@angular/router';
     styles: ``,
 })
 export class PlaysComponent {
-    plays = MOCK_PLAYS;
+    plays: Play[] = [];
+    
+    constructor(
+        private playService: PlayService
+    ) {}
 
+    getPlays(): void {
+        this.playService.all().subscribe(dataPackage => this.plays = <Play[]> dataPackage.data);
+    }
 
+    ngOnInit() {
+        this.getPlays();
+    }
 }
