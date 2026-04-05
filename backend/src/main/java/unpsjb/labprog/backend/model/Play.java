d74c12275be3670bdea8651b5587097064843cf5
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-04 21:34:10 -0300

    modifies atribute type, changes from String to PlayType

--- backend/src/main/java/unpsjb/labprog/backend/model/Play.java
+++ backend/src/main/java/unpsjb/labprog/backend/model/Play.java
@@ -3,6 +3,8 @@ package unpsjb.labprog.backend.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
@@ -27,6 +29,8 @@ public class Play {
 
     private String name;
 
-    private String type;
+    @ManyToOne
+    @JoinColumn(name = "play_type_id")
+    private PlayType type;
     
 }
