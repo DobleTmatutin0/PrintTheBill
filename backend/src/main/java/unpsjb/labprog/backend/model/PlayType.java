2670b80a4c6f8a813c6257ea7d1cf96a1ba4e586
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-04 21:33:24 -0300

    adds PlayType class and his rep, svc and presenter

---
+++ backend/src/main/java/unpsjb/labprog/backend/model/PlayType.java
@@ -0,0 +1,28 @@
+package unpsjb.labprog.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class PlayType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(unique = true)
+    private String type;
+    
+}
