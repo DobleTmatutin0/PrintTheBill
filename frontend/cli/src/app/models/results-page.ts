1a5534439897a346e8f85280a206f6add21b5e8f
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-12 22:08:09 -0300

    adds model of resulst page for pagination

---
+++ frontend/cli/src/app/models/results-page.ts
@@ -0,0 +1,10 @@
+export interface ResultsPage {
+    content: any[];
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    last: boolean;
+    numberOfElements: number;
+    size: number;
+    number: number;
+}
