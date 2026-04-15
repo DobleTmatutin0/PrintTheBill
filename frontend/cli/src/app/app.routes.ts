f501203c25b222f67fa7f22ec44267830680669e
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-14 22:58:26 -0300

    adds route to bordero.component

--- frontend/cli/src/app/app.routes.ts
+++ frontend/cli/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home.component';
 import { PlaysComponent } from './components/plays.component/plays.component';
 import { PlaysDetailComponent } from './components/plays.component/plays-detail.component';
+import { BorderoDetailComponent } from './components/bordero-detail.component/bordero-detail.component/bordero-detail.component';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'plays', component: PlaysComponent },
-    { path: 'plays/:code', component: PlaysDetailComponent }
+    { path: 'plays/:code', component: PlaysDetailComponent },
+    { path: 'borderos/:id', component: BorderoDetailComponent }
 ];
