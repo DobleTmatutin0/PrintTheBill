6c848217070d7713b216b709738738e05135792b
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-14 23:02:28 -0300

    adds bordero's component

---
+++ frontend/cli/src/app/components/bordero-detail.component/bordero-detail.component/bordero-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bordero-detail.component',
+  imports: [],
+  templateUrl: 'bordero-detail.component.html',
+  styles: ``,
+})
+export class BorderoDetailComponent {
+
+}
