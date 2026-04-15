220bc03c457370712754b3d51809e0a2e1f64196
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-14 23:00:01 -0300

    adds bordero's required models

---
+++ frontend/cli/src/app/models/bordero.ts
@@ -0,0 +1,9 @@
+import { Customer } from "./customer";
+import { Performance } from "./performance";
+
+export interface Bordero {
+    id: number;
+    date: Date;
+    customer: Customer;
+    performances: Performance[];
+}
---
+++ frontend/cli/src/app/models/performance.ts
@@ -0,0 +1,7 @@
+import { Play } from "./play";
+
+export interface Performance {
+    id: number;
+    audience: number;
+    play: Play;
+}
