45e691b7779267bd72df2590b2b0c2e6d06639b6
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-14 21:54:37 -0300

    changes imports to match the new component's routes

--- frontend/cli/src/app/app.routes.ts
+++ frontend/cli/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home.component';
-import { PlaysComponent } from './plays.component/plays.component';
-import { PlaysDetailComponent } from './plays.component/plays-detail.component';
+import { PlaysComponent } from './components/plays.component/plays.component';
+import { PlaysDetailComponent } from './components/plays.component/plays-detail.component';
 
 export const routes: Routes = [
     { path: '', component: Home },
