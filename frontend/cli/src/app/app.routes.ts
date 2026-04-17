dc155348ff458ee39bcf9c9e005e04f119b1cb29
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-16 22:11:15 -0300

    minor change

--- frontend/cli/src/app/app.routes.ts
+++ frontend/cli/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home.component';
 import { PlaysComponent } from './components/plays.component/plays.component';
 import { PlaysDetailComponent } from './components/plays.component/plays-detail.component';
-import { BorderoDetailComponent } from './components/bordero-detail.component/bordero-detail.component/bordero-detail.component';
+import { BorderoDetailComponent } from './components/bordero-detail.component/bordero-detail.component';
 
 export const routes: Routes = [
     { path: '', component: Home },
