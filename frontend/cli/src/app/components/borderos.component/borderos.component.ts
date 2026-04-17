24fcc9e11cfa506d0ba35140184adf6c18334ce0
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-17 00:02:06 -0300

    adds bordero component

---
+++ frontend/cli/src/app/components/borderos.component/borderos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bordero.component',
+  imports: [],
+  template: ` <p>bordero.component works!</p> `,
+  styles: ``,
+})
+export class BorderosComponent {}
