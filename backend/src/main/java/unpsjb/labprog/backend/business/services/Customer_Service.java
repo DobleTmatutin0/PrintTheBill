944a0374f99b8c13f6e69ab25094c08e2f3098c6
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-17 11:50:44 -0300

    adds create and delete methods

--- backend/src/main/java/unpsjb/labprog/backend/business/services/Customer_Service.java
+++ backend/src/main/java/unpsjb/labprog/backend/business/services/Customer_Service.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import unpsjb.labprog.backend.business.repositories.Customer_Repository;
 import unpsjb.labprog.backend.model.Customer;
@@ -24,4 +25,15 @@ public class Customer_Service {
     public List<Customer> search(String term) {
         return customer_Repository.search("%" + term + "%");
     }
+
+    @Transactional
+    public Customer save(Customer aCustomer) {
+        return this.customer_Repository.save(aCustomer);
+    }
+
+    @Transactional
+    public void delete(int aCustomerId) {
+        this.customer_Repository.deleteById(aCustomerId);
+    }
+
 }
--- backend/src/main/java/unpsjb/labprog/backend/presenter/Customer_Presenter.java
+++ backend/src/main/java/unpsjb/labprog/backend/presenter/Customer_Presenter.java
@@ -2,14 +2,18 @@ package unpsjb.labprog.backend.presenter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import unpsjb.labprog.backend.Response;
+import unpsjb.labprog.backend.model.Customer;
 import unpsjb.labprog.backend.business.services.Customer_Service;
+import unpsjb.labprog.backend.Response;
 
 @RestController
 @RequestMapping("customers")
@@ -31,4 +35,16 @@ public class Customer_Presenter {
     public ResponseEntity<Object> search(@PathVariable("term") String term) {
         return Response.ok(customer_Service.search(term));
     }
+
+    @PostMapping()
+    public ResponseEntity<Object> create(@RequestBody Customer aCustomer) {
+        return Response.ok((customer_Service.save(aCustomer)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable("id") int id) {
+        this.customer_Service.delete(id);
+        return Response.ok("the customer with id " + id + " has been successfully deleted");
+    }
+
 }
