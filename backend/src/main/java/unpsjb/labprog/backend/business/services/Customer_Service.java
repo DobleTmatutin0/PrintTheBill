819d923068d5f7701ca5cb9681e56f8b6025ec24
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-17 00:06:53 -0300

    adds findByPage method

--- backend/src/main/java/unpsjb/labprog/backend/business/services/Customer_Service.java
+++ backend/src/main/java/unpsjb/labprog/backend/business/services/Customer_Service.java
@@ -3,6 +3,8 @@ package unpsjb.labprog.backend.business.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import unpsjb.labprog.backend.business.repositories.Customer_Repository;
@@ -14,6 +16,10 @@ public class Customer_Service {
 
     @Autowired
     private Customer_Repository customer_Repository;
+
+    public Page<Customer> findByPage(int page, int size) {
+        return customer_Repository.findAll(PageRequest.of(page, size));
+    }
 
     public List<Customer> search(String term) {
         return customer_Repository.search("%" + term + "%");
--- backend/src/main/java/unpsjb/labprog/backend/presenter/Customer_Presenter.java
+++ backend/src/main/java/unpsjb/labprog/backend/presenter/Customer_Presenter.java
@@ -5,18 +5,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import unpsjb.labprog.backend.Response;
 import unpsjb.labprog.backend.business.services.Customer_Service;
 
 @RestController
-@RequestMapping("customer")
+@RequestMapping("customers")
 
 public class Customer_Presenter {
 
     @Autowired
     private Customer_Service customer_Service;
+
+    @GetMapping("/page")
+    public ResponseEntity<Object> findByPage(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        return Response.ok(customer_Service.findByPage(page, size));
+    }
 
     @GetMapping("/search/{term}")
     public ResponseEntity<Object> search(@PathVariable("term") String term) {
