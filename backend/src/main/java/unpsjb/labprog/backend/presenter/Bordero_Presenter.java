4175f5c74e74f86ea1c46b7242f09aed63cbcf8b
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-17 09:57:14 -0300

    adds findByPage, update and delete endpoints

--- backend/src/main/java/unpsjb/labprog/backend/presenter/Bordero_Presenter.java
+++ backend/src/main/java/unpsjb/labprog/backend/presenter/Bordero_Presenter.java
@@ -2,11 +2,14 @@ package unpsjb.labprog.backend.presenter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import unpsjb.labprog.backend.Response;
@@ -26,6 +29,14 @@ public class Bordero_Presenter {
        return Response.ok(bordero_Service.findAll()); 
     }
 
+    @GetMapping("/page")
+    public ResponseEntity<Object> findByPage(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        return.Response.ok(bordero_Service.findByPage(page, size));
+    }
+
     @GetMapping("/id/{id}")
     public ResponseEntity<Object> findById(@PathVariable("id") int id) {
         Bordero aBorderoOrNull = bordero_Service.findById(id);
@@ -38,4 +49,22 @@ public class Bordero_Presenter {
     public ResponseEntity<Object> create(@RequestBody Bordero aBordero) {
         return Response.ok(bordero_Service.save(aBordero), "Bordero creado correctamente");
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@RequestBody Bordero aBordero) {
+        if (aBordero.getId() <= 0) {
+            return Response.error(
+                aBordero,
+                "Invalid ID"
+            );
+        }
+        return Response.ok(bordero_Service.save(aBordero));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable("id") int id) {
+        bordero_Service.delete(id);
+        return Response.ok("The bordero with id \" + id + \" has been successfully deleted");
+    }
+
 }
