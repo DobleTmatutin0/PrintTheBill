f4ae6564495be69dc6e36e4cd0937523d0658ce4
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-17 00:03:38 -0300

    adds routes to customers and borderos

--- frontend/cli/src/app/app.routes.ts
+++ frontend/cli/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Home } from './home/home.component';
 import { PlaysComponent } from './components/plays.component/plays.component';
 import { PlaysDetailComponent } from './components/plays.component/plays-detail.component';
 import { BorderoDetailComponent } from './components/bordero-detail.component/bordero-detail.component';
+import { CustomersComponent } from './components/customers.component/customers.component';
+import { BorderosComponent } from './components/borderos.component/borderos.component';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'plays', component: PlaysComponent },
     { path: 'plays/:code', component: PlaysDetailComponent },
+    { path: 'customers', component: CustomersComponent },
+    { path: 'borderos', component: BorderosComponent },
     { path: 'borderos/:id', component: BorderoDetailComponent }
 ];
